6f12857a7dd7b123e87d8158090fb70269872117
Vicky Zheng <vickyjmzheng@gmail.com>
2020-09-25 22:54:14 -0400

    finished app

---
+++ DemoApp/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  DemoApp
+//
+//  Created by Vicky Zheng on 9/25/20.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
