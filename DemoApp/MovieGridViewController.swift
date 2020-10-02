9ae3d59c4c6aebc95ffcbdbd3b46f0f45ea08658
Vicky Zheng <vickyjmzheng@gmail.com>
2020-10-02 19:30:49 -0400

    Added functionality so that we can see movie details from the grid view

--- DemoApp/MovieGridViewController.swift
+++ DemoApp/MovieGridViewController.swift
@@ -66,14 +66,22 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
         return cell
     }
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let cell = sender as! UICollectionViewCell
+        let indexPath = collectionView.indexPath(for: cell)!
+        let movie = movies[indexPath.item]
+        
+        // Pass the selected movie to the details view controller
+        let detailsViewController = segue.destination as! MovieGridDetailsViewController
+        detailsViewController.movie = movie
+        
+        collectionView.deselectItem(at: indexPath, animated: true)
+        
     }
-    */
+    
 
 }
